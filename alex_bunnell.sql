8530a68c741ed56b16297ade2905b881913e51fd
Cheshire Cat <thacheshirecat@gmail.com>
2018-07-18 18:27:22 -0700

    Add sql files as opposed to zipped sql files to Solution directory.

--- alex_bunnell.sql
+++ alex_bunnell.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 19, 2018 at 01:18 AM
+-- Generation Time: Jul 19, 2018 at 01:26 AM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.1.5
 
@@ -37,21 +37,6 @@ CREATE TABLE `clients` (
   `category_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `clients`
---
-
-INSERT INTO `clients` (`id`, `name`, `phone`, `category_id`) VALUES
-(15, 'Tim Tim', '703 780 1234', 12),
-(16, 'Barny Sam', '703 780 1235', 12),
-(17, 'Timmy Toms', '703 780 1236', 12),
-(18, 'Tiny Tina', '703 780 1237', 14),
-(19, 'Reggie', '703 780 1238', 13),
-(20, 'John', '703 780 1239', 12),
-(21, 'Rachel', '703 780 1240', 14),
-(22, 'Barnaby Jones', '703 780 1241', 13),
-(23, 'Benny Benners', '703 780 1242', 14);
-
 -- --------------------------------------------------------
 
 --
@@ -63,15 +48,6 @@ CREATE TABLE `stylists` (
   `name` varchar(255) DEFAULT NULL,
   `style` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `stylists`
---
-
-INSERT INTO `stylists` (`id`, `name`, `style`) VALUES
-(12, 'John', 'Lumberjack Haircuts'),
-(13, 'Rex', '80s Big Hair'),
-(14, 'Shawna', 'Straightning, Curling, Highilights');
 
 --
 -- Indexes for dumped tables
