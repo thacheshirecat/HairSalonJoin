2b7203ec1ab8520540971c6bf459f410572174f9
Cheshire Cat <thacheshirecat@gmail.com>
2018-07-20 13:57:25 -0700

    Update Stylist views and controller to handle new functionality.

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -32,7 +32,39 @@ namespace HairSalon.Controllers
     [HttpGet("/Stylist/{id}/View")]
     public ActionResult View(int id)
     {
-      return View(Stylist.Search(id));
+      Dictionary<string, object> model = new Dictionary<string, object>();
+
+      Stylist selectedStylist = Stylist.Search(id);
+      List<Client> stylistsClients = Client.SearchClientsByStylist(id);
+      List<Specialty> stylistsSpecialties = selectedStylist.GetAllSpecialties();
+      List<Specialty> allSpecialties = Specialty.GetAll();
+
+      model.Add("stylist", selectedStylist);
+      model.Add("clients", stylistsClients);
+      model.Add("specialties", stylistsSpecialties);
+      model.Add("allspecials"), allSpecialties);
+
+      return View(model);
+    }
+    [HttpPost("/Stylist/AddSpecialty")]
+    public ActionResult AddSpecialty(int stylistid, int newspecialtyid)
+    {
+
+      Dictionary<string, object> model = new Dictionary<string, object>();
+
+      Stylist selectedStylist = Stylist.Search(stylistid);
+      Specialty newSpecialty = Specialty.Search(newspecialtyid);
+      selectedStylist.AddSpecialty(newSpecialty);
+      List<Client> stylistsClients = Client.SearchClientsByStylist(stylistid);
+      List<Specialty> stylistsSpecialties = selectedStylist.GetAllSpecialties();
+      List<Specialty> allSpecialties = Specialty.GetAll();
+
+      model.Add("stylist", selectedStylist);
+      model.Add("clients", stylistsClients);
+      model.Add("specialties", stylistsSpecialties);
+      model.Add("allspecials"), allSpecialties);
+
+      return View("View", model);
     }
   }
 }
